9ac0914d41f8d0dd9b2bd42d24f7ed2a00b9502d
KevinWJohnson <kevin.johnson3@cox.net>
2018-07-28 17:30:01 -0700

    Fixed errors with the two model files character.js and user.js - replaced Sequelized with DataTypes. Also added a password and validation code to the user.js file.

--- models/character.js
+++ models/character.js
@@ -3,22 +3,22 @@ module.exports = function (sequelize, DataTypes) {
   var Character = sequelize.define("Character", {
     // the name of the character (a string)
     character_name: {
-      type: Sequelize.STRING,
+      type: DataTypes.STRING,
       allowNull: false,
     },
     // the character's gender (a string)
     gender: {
-      type: Sequelize.STRING,
+      type: DataTypes.STRING,
       allowNull: false,
     },
     // the character's health (an integer)
     health: {
-      type: Sequelize.INTEGER,
+      type: DataTypes.INTEGER,
       defaultValue: 0
     },
     // the character's level (an integer)
     character_level: {
-      type: Sequelize.INTEGER,
+      type: DataTypes.INTEGER,
       defaultValue: 0
     }
   });
--- models/user.js
+++ models/user.js
@@ -1,11 +1,43 @@
 
+import bcrypt from 'bcrypt-nodejs'
+
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define("User", {
         // the name of the character (a string)
         user_name: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
     });
     return User;
+};
+
+
+User.beforeCreate(function (user, options) {
+    return cryptPassword(user.password)
+        .then(success => {
+            user.password = success;
+        })
+        .catch(err => {
+            if (err) console.log(err);
+        });
+});
+
+function cryptPassword(password) {
+    console.log("cryptPassword" + password);
+    return new Promise(function (resolve, reject) {
+        bcrypt.genSalt(10, function (err, salt) {
+            // Encrypt password using bycrpt module
+            if (err) return reject(err);
+
+            bcrypt.hash(password, salt, null, function (err, hash) {
+                if (err) return reject(err);
+                return resolve(hash);
+            });
+        });
+    });
 };
