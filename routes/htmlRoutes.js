2d45d2c668bfd02ff4e04f3707d0f3c01f5c6091
KevinWJohnson <kevin.johnson3@cox.net>
2018-07-31 12:46:34 -0700

    htmlRoutes.js modified

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,24 @@
 var db = require("../models");
 var path = require("path");
 module.exports = function(app) {
-
   app.get("/add", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/add.html"));
+  });
+
+  app.get("/browse", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/browse.html"));
+  });
+
+  app.get("/cookbook", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/cookbook.html"));
+  });
+
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/viewRecipe", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/viewRecipe.html"));
   });
 
   app.get("*", function(req, res) {
