be7a55acff17556fe5dfc821b9e662cbceb597d1
Etch <Etch.rush@gmail.com>
2018-07-31 11:15:22 -0800

    add page started

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,6 +15,12 @@ module.exports = function(app) {
     });
   });
 
+  app.post("/api/recipes", function(req, res) {
+    db.User.create(req.body).then(function(dbExample) {
+      res.json(dbExample);
+    });
+  });
+
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
